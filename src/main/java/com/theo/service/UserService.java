a771209fbf516e41e227ba3dea83083598667724
yanshihao <yanshihao@wayeasoft.com>
2019-03-27 22:40:49 +0800

    调整

--- src/main/java/com/theo/service/UserService.java
+++ src/main/java/com/theo/service/UserService.java
@@ -2,8 +2,12 @@ package com.theo.service;
 
 import com.theo.entity.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User getUser(Long id);
+
+    List<User> getUsers();
 
 }
--- src/main/java/com/theo/service/impl/UserServiceImpl.java
+++ src/main/java/com/theo/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 @Transactional
@@ -22,5 +23,10 @@ public class UserServiceImpl implements UserService {
         User user = userDao.selectById(id);
 
         return user;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userDao.getAll();
     }
 }
